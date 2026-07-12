e212d3c942e8bdf5dcc2a700a772160a14d54593
iky-art <gtau22609@gmail.com>
2026-07-13 01:07:13 +0700

    Update version and enhance load_config function
    Updated the default version in the configuration and modified the load_config function to ensure the config file is created if it doesn't exist.

--- core/config.py
+++ core/config.py
@@ -5,7 +5,7 @@ CONFIG_FILE = "data/config.json"
 
 DEFAULT_CONFIG = {
     "name": "Apollo Framework System",
-    "version": "0.4.0",
+    "version": "0.5.0",
     "theme": "dark",
     "language": "id",
     "workspace": "workspace",
@@ -16,20 +16,19 @@ DEFAULT_CONFIG = {
 
 def load_config():
 
+    os.makedirs("data", exist_ok=True)
+
     if not os.path.exists(CONFIG_FILE):
 
-        os.makedirs("data", exist_ok=True)
-
-        with open(CONFIG_FILE, "w") as f:
-            json.dump(DEFAULT_CONFIG, f, indent=4)
-
-        return DEFAULT_CONFIG
+        save_config(DEFAULT_CONFIG)
 
     with open(CONFIG_FILE, "r") as f:
+
         return json.load(f)
 
 
 def save_config(config):
 
     with open(CONFIG_FILE, "w") as f:
+
         json.dump(config, f, indent=4)
